ef38c9a37eb93769a8a14110ed4ec5c26d99aff7
nadrojb <jordanbrewer1994@gmail.com>
2025-01-14 11:48:12 +0000

    in the process of creating new route for login

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,12 @@ use App\Http\Controllers\InvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/login', [Authcont])
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
 
 Route::get('/customers', [CustomerController::class, 'index']);
 Route::get('/customer/{customer}', [CustomerController::class, 'show']);
