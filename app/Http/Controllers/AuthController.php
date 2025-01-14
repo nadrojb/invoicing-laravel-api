09a0f2ee92394b77e6bf105395d3843e2f6d6fc2
nadrojb <jordanbrewer1994@gmail.com>
2025-01-14 13:44:10 +0000

    created new register route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
+use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
-    public function login () {
-        return response()->json([
-            'message'=>'Hello Login!'
-        ], 200);
+    use ApiResponses;
+    public function login (ApiLoginRequest $request): JsonResponse
+    {
+        return $this->ok($request->get('email'));
+    }
+
+    public function register(): JsonResponse
+    {
+        return $this->ok('register');
     }
 }
