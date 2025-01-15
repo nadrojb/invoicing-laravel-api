f0464234f3eebf49dcc2c29102b4637863571955
nadrojb <jordanbrewer1994@gmail.com>
2025-01-15 10:59:23 +0000

    removed code that's not needed in invoice model

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\V1\InvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/invoices', InvoiceController::class);
+Route::middleware('auth:sanctum')->apiResource('/invoices', InvoiceController::class); //ensure in our invoice resources the request must include a sanctum token
 
 Route::get('/user', function (Request $request) {
     return $request->user();
