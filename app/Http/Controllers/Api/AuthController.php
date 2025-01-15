c5f6c739228a3c12039ea4e478122a93aa0bdb28
nadrojb <jordanbrewer1994@gmail.com>
2025-01-15 10:35:18 +0000

    tokens are now being created successfully for users loggin in

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,15 +4,34 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\LoginUserRequest;
+use App\Models\User;
 use App\Traits\ApiResponses;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     use ApiResponses;
-    public function login (LoginUserRequest $request): JsonResponse
+
+    public function login(LoginUserRequest $request): JsonResponse
     {
-        return $this->ok($request->get('email'));
+       $request->validated($request->all());//validated returns an array of the data that was successfully validated from the form request.
+
+        if(!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('Invalid credentials', 401);
+        }
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return $this->error('User not found', 404);
+        }
+
+        return $this->ok(
+            'Authenticated',
+            [
+                'token' => $user->createToken('API token for' . $user->email)->plainTextToken
+            ]
+        );
     }
 
     public function register(): JsonResponse
--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo
 
 class Invoice extends Model
 {
     /** @use HasFactory<\Database\Factories\InvoiceFactory> */
     use HasFactory;
 
-    public function customer()
+    public function customer():
     {
         return $this->belongsTo(Customer::class);
     }
