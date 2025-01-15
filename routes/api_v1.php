23f4598cd7015a1a566e4329598816b3e2149d01
nadrojb <jordanbrewer1994@gmail.com>
2025-01-15 14:24:22 +0000

    conditionally including data using merge when

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\V1\InvoiceController;
+use App\Http\Controllers\Api\V1\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->apiResource('/invoices', InvoiceController::class); //ensure in our invoice resources the request must include a sanctum token
+Route::middleware('auth:sanctum')->apiResource('/users', UsersController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
