309dc7afa5ca8bb67b9ed427e1c7809b6f0a621f
nadrojb <jordanbrewer1994@gmail.com>
2025-01-28 13:37:17 +0000

    changed the authors invoice controller query to fetch the invoices where the foreign key matches

--- app/Http/Controllers/Api/V1/AuthorsInvoicesController.php
+++ app/Http/Controllers/Api/V1/AuthorsInvoicesController.php
@@ -1,14 +1,14 @@
 <?php
-//
-//namespace App\Http\Controllers\Api\V1;
-//
-//use App\Http\Controllers\Controller;
-//use App\Http\Resources\V1\InvoiceResource;
-//use App\Models\Invoice;
-//
-//class AuthorsInvoicesController extends Controller
-//{
-//    public function index($user_id) {
-//        return InvoiceResource::collection(Invoice::where('id', $user_id)->paginate());
-//    }
-//}
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Models\Invoice;
+
+class AuthorsInvoicesController extends Controller
+{
+    public function index($user_id) {
+        return InvoiceResource::collection(Invoice::where('user_id', $user_id)->paginate());
+    }
+}
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->apiResource('/invoices', InvoiceController::class); //ensure in our invoice resources the request must include a sanctum token
 Route::middleware('auth:sanctum')->apiResource('/authors', AuthorsController::class);
-//Route::middleware('auth:sanctum')->apiResource('/users.invoices', AuthorsInvoicesController::class);
+Route::middleware('auth:sanctum')->apiResource('/authors.invoices', AuthorsInvoicesController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
