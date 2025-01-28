c5f3f9b89ccaf6b1eba4bfb7719a05b8ff035f75
nadrojb <jordanbrewer1994@gmail.com>
2025-01-28 11:30:31 +0000

    can now filter invoices by customer name includes

--- app/Http/Filters/V1/InvoiceFilter.php
+++ app/Http/Filters/V1/InvoiceFilter.php
@@ -2,8 +2,22 @@
 
 namespace App\Http\Filters\V1;
 
-class InvoiceFilter extends QueryFilter {
-    public function status($value) {
-        return $this->builder->where('status', $value);
+class InvoiceFilter extends QueryFilter
+{
+    public function include($value)
+    {
+        return $this->builder->with($value);
+    }
+
+    public function status($value)
+    {
+        //Below is seperating multiple query parameters into an array
+        return $this->builder->whereIn('status', explode(',', $value));
+    }
+
+    public function customerName($value)
+    {
+        $likeStr = str_replace('*', '%', $value);
+        return $this->builder->where('customer_name', 'like', $likeStr);
     }
 }
